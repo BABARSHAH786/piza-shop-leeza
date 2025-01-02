735c210b160a703146acd760acf9c6215ca1de56
BABARSHAH786 <syedbabarali640@gmail.com>
2025-01-03 02:58:56 +0500

    fir pizaa shop t

--- src/components/TopSide.tsx
+++ src/components/TopSide.tsx
@@ -2,14 +2,14 @@ import Image from "next/image"
 export default function TopSide(){
     return(
         <>
-        <div className="max-md:grid max-ss:grid flex mt-4 ml-4">
-            <div className="w-1/3 h-[340px] max-ss:w-full max-md:w-full ">
-                <Image src={"/img2.jpeg"} alt="" width={450} height={250} />
+        <div className="max-md:grid max-ss:grid  mt-4 ml-4">
+            <div className="w-1/3 h-[340px] ml-[340px] max-ss:w-full max-md:w-full ">
+                <Image src={"/PIZZA.jpeg"} alt="" width={450} height={250} />
         
              </div>
              <div className="max-md:w-[390px] max-2xl:w-1/2 ">
-                <h2 className="text-[28px] font-bold ">Blogging website</h2> 
-                <h2 className="text-[20px] ">TypeScript integrates seamlessly with Next.js by adding static typing to components, pages, API routes, and data structures. 1  This enables compile-time error checking, improves code maintainability and refactoring, and enhances the developer experience through better code completion and type inference</h2>
+                <h2 className="text-[28px] font-bold ml-6">PIZZA SHOP </h2> 
+                <h2 className="text-[20px] ml-6 ">Pizza is one of the world's most beloved dishes, and for good reason. This savory treat combines the perfect blend of flavors, textures, and aromas, making it a staple in many cuisines. From classic Neapolitan-style pizzas with fresh tomatoes and mozzarella, to gourmet pies loaded with unique toppings, there's a pizza out there for everyone.</h2>
         
              </div>
         
