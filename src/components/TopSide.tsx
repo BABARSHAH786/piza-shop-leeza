10f6c754c3af7aab5c37383296c3c01873dd4c67
BABARSHAH786 <syedbabarali640@gmail.com>
2025-01-03 03:12:13 +0500

    update code sanityiii

--- src/components/TopSide.tsx
+++ src/components/TopSide.tsx
@@ -9,7 +9,7 @@ export default function TopSide(){
              </div>
              <div className="max-md:w-[390px] max-2xl:w-1/2 ">
                 <h2 className="text-[28px] font-bold ml-6">PIZZA SHOP </h2> 
-                <h2 className="text-[20px] ml-6 ">Pizza is one of the world's most beloved dishes, and for good reason. This savory treat combines the perfect blend of flavors, textures, and aromas, making it a staple in many cuisines. From classic Neapolitan-style pizzas with fresh tomatoes and mozzarella, to gourmet pies loaded with unique toppings, there's a pizza out there for everyone.</h2>
+                <h2 className="text-[20px] ml-6 ">Pizza is one of the world s most beloved dishes, and for good reason. This savory treat combines the perfect blend of flavors, textures, and aromas, making it a staple in many cuisines. From classic Neapolitan-style pizzas with fresh tomatoes and mozzarella, to gourmet pies loaded with unique toppings, theres a pizza out there for everyone.</h2>
         
              </div>
         
